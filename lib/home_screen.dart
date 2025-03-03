19ef73311496f195b6a80a997c77ceacb45d362b
arpon-jolas-santos <jolasarpon1@gmail.com>
2025-03-03 19:36:30 +0800

    editted by jolas

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,6 +11,75 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+      return CupertinoPageScaffold(
+    child: Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+           color: Colors.black
+          ),
+          child: SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 80),
+                  Text(
+                    "Welcome to Spinner!",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color:  CupertinoColors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 15),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Not sure what to choose? Let Spinner decide for you!",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color:  CupertinoColors.systemGrey4,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  
+                  const SizedBox(height: 15),
+                  Text(
+                    "Click the wheel to start!",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: CupertinoColors.systemGrey4,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 50),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      "This app helps you make quick decisions by randomly selecting from your provided options. Whether it’s picking a meal, choosing a movie, or making any random decision, Spinner has got you covered!",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: CupertinoColors.systemGrey3 ,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(height: 150),
+                ],
+              ),
+            ),
+          ),
+        ),
+       
+      ],
+    ),
+  );
 }
+  }
