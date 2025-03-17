eae8e0437a07c7257c5efa0524e6506f54f6dc74
beyonce-ama <amabey222@gmail.com>
2025-03-17 21:16:36 +0800

    add config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,16 @@
+def keystoreProperties = new Properties()
+if (System.getenv("CM_KEYSTORE")) {
+    keystoreProperties["storeFile"] = "codemagic.keystore"
+    keystoreProperties["storePassword"] = System.getenv("CM_KEYSTORE_PASSWORD")
+    keystoreProperties["keyAlias"] = System.getenv("CM_KEY_ALIAS")
+    keystoreProperties["keyPassword"] = System.getenv("CM_KEY_PASSWORD")
+} else {
+    def keystorePropertiesFile = rootProject.file("key.properties")
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
+    }
+}
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -19,6 +32,20 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+  signingConfigs {
+        release {
+            storeFile file("/tmp/keystore.jks")
+            storePassword System.getenv("CM_KEYSTORE_PASSWORD")
+            keyAlias System.getenv("CM_KEY_ALIAS")
+            keyPassword System.getenv("CM_KEY_PASSWORD")
+        }
+    }
+    buildTypes {
+        release {
+            signingConfig signingConfigs.release
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.spinner"
@@ -30,13 +57,6 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
 }
 
 flutter {
